2b868e02f004a53f7bdb0ca8c1ca6e051bfed98e
kirthiga89P <121348734+kirthiga89P@users.noreply.github.com>
2023-05-04 10:24:53 +0530

    Added Comments

--- django/eventproj/event/seriallizer.py
+++ django/eventproj/event/seriallizer.py
@@ -3,7 +3,7 @@ from .models import EventDetails,eventUser
 from drf_extra_fields.fields import Base64ImageField
 
 class url:   
-
+   #To get the image from the media folder
     def get_imageurl(self,EventDetails):
         
         
@@ -15,14 +15,16 @@ class url:
         return "http://127.0.0.1:8000/"+image 
 
 
-
+#Serializer to Create a Event
 class EventCreateSerializer(serializers.ModelSerializer,url):
+    #To store image in a database
     image=serializers.ImageField()
     
     class Meta:
         
         model=EventDetails
         fields = ('__all__')
+#Serailizer to view the Event with the liked details        
 class EventviewSerializer(serializers.ModelSerializer,url):
     is_liked=serializers.BooleanField()
     image=serializers.SerializerMethodField("get_imageurl")
@@ -30,6 +32,7 @@ class EventviewSerializer(serializers.ModelSerializer,url):
         
         model=EventDetails
         fields = ('id','event_name','date','time','location','is_liked','image')
+ #Serializer to view the event liked by User       
 class EventUserSerializer(serializers.ModelSerializer,url):   
     
     class Meta:
